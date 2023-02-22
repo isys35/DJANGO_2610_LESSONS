c8f50ad382bcb4870b79d3edafd75742fbe5d650
isys35 <isys35@mail.ru>
2023-02-22 21:08:31 +0300

    fix: form without model

--- classroom/courses/forms.py
+++ classroom/courses/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class CourseForm(forms.Form):
     name = forms.CharField(label="Название курса", max_length=100)
-    description = forms.CharField(label="Описание")
-    started_at = forms.DateField(label="Старт курса")
+    description = forms.CharField(label="Описание", required=False)
+    started_at = forms.DateField(label="Старт курса", widget=forms.DateInput)
--- classroom/courses/views.py
+++ classroom/courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from courses import forms
+from courses import models
 
 def courses_list(request):
     return render(request, "courses/list.html")
@@ -9,7 +10,9 @@ def course_create(request):
     if request.method == "POST":
         form = forms.CourseForm(request.POST)
         if form.is_valid():
-            print("Форма валидна")
+            models.Course.objects.create(**form.cleaned_data)
             return redirect("courses:list")
+        else:
+            return render(request, "courses/create.html", context={"form": form})
     form = forms.CourseForm()
     return render(request, "courses/create.html", context={"form": form})
